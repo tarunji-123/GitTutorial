2bfdfdfa02fd77262c662a1a3baa1807488bb3e5
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-15 18:31:51 +0530

    DOM Manipulate

--- demo2.js
+++ demo2.js
@@ -1,7 +1,7 @@
-document.title = 'my new title';
+// document.title = 'my new title';
 
-var header =document.getElementById('main-header');
-header.style.border = 'solid 2px black';
+// var header =document.getElementById('main-header');
+// header.style.border = 'solid 2px black';
 
 // var title = document.getElementsByClassName('title');
 // title[0].style.fontWeight = 'bold';
@@ -22,16 +22,54 @@ header.style.border = 'solid 2px black';
 // }
 
 // get element by query selector
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = "green";
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = "green";
 
 // var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
 // thirdItem.style.visibility = "hidden";
 
 // get element by queryselectorAll
-var items = document.querySelectorAll('li');
-items[1].style.color = "yellow";
+// var items = document.querySelectorAll('li');
+// items[1].style.color = "yellow";
 
-for(var i=0; i<items.length; i+=2){
-    items[i].style.color = "green";
-}
+// for(var i=0; i<items.length; i+=2){
+//     items[i].style.color = "green";
+// }
+
+//Traversing the dom
+var itemList = document.querySelector('#items');
+// Parent Element
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = "grey";
+
+// child nodes
+// console.log(itemList.lastElementChild);
+itemList.lastElementChild.style.color = "blue";
+console.log(itemList.lastChild);// it gives text because of space.
+
+itemList.firstElementChild.style.color = "red";
+console.log(itemList.firstChild);
+
+console.log(itemList.nextSibling);
+itemList.nextElementSibling.style.backgroundColor = "aqua";
+
+console.log(itemList.previousSibling);
+itemList.previousElementSibling.style.backgroundColor= "green";
+
+// create Element
+//create a div
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';// add class name 
+newDiv.id = 'hello1'; //add id
+newDiv.setAttribute('title','Hello new div created');
+
+var newDivText = document.createTextNode('Hello World');
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize = "50px";
+container.insertBefore(newDiv,h1);
