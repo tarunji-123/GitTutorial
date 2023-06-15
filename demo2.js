f9a2dacd34ff66fb045a481f744da95dc966ee40
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-15 15:32:33 +0530

    use getElementByClassName on items

--- demo2.js
+++ demo2.js
@@ -3,10 +3,17 @@ document.title = 'my new title';
 var header =document.getElementById('main-header');
 header.style.border = 'solid 2px black';
 
-var title = document.getElementsByClassName('title');
+// var title = document.getElementsByClassName('title');
 
-title[0].style.fontWeight = 'bold';
+// title[0].style.fontWeight = 'bold';
 
-title[0].style.color = 'green';
+// title[0].style.color = 'green';
 
-console.log("good");
+// Get element by class name
+
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = "green";
+
+for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight = "bold";
+}
