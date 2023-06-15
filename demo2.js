b604d94424dc86d3c2a8d59accbb71015a430b9d
tarunji-123 <tarunbhadoriya141@gmail.com>
2023-06-15 13:21:07 +0530

    intial commit

---
+++ demo2.js
@@ -0,0 +1,10 @@
+document.title = 'my new title';
+
+var header =document.getElementById('main-header');
+header.style.border = 'solid 2px black';
+
+var title = document.getElementsByClassName('title');
+
+title[0].style.fontWeight = 'bold';
+
+title[0].style.color = 'green';
